b998d07c78d075efd32d7350d6e49be9f0ad3f33
ChrissJones <94918921+ChrissJones@users.noreply.github.com>
2022-02-07 23:53:47 -0500

    Add files via upload
    jscodingchallenge5

---
+++ Udemyjscodealong/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// prettier-ignore 
+const months = ['January', 'Frebruary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;  
+      const { longitude } = position.coords;
+      console.log( 'https://www.google.com/maps/@{latitude},${longitude}');
+    
+      const coords = [latitude, longitude]
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+    }).addTo(map);
+
+L.marker([coords, -0.09])
+.addTo(map);
+
+L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+
+  console.log(firstName);
+
